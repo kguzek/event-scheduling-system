45f25189fba48ff1f44cb601b7325f06e56e885b
Konrad Guzek <konrad@guzek.uk>
2024-09-12 19:01:19 +0200

    implement login/signup/reset Swing front-end interface

---
+++ client/src/main/java/uk/guzek/ess/client/error/web/body/ErrorResponse.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uk.guzek.ess.client.error.web.body;
+
+import lombok.Data;
+
+/**
+ *
+ * @author konrad
+ */
+@Data
+public class ErrorResponse {
+    private String message;
+}
